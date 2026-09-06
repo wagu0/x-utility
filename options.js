dc44d9a8ceff3bc8c89948b430549f5251825539
wagu0 <23jn0301@jec.ac.jp>
2026-09-06 10:06:08 +0900

    -feature 修飾キーの登録を無視、escでのキャンセル実装、クリアボタンの実装、重複キーの登録不可 -refactor uiの調整

--- options.js
+++ options.js
@@ -1,11 +1,14 @@
 // 定数
 const RECORD_BUTTON_CLASS = ".record-button";
 const SHORTCUT_ITEM_CLASS = ".shortcut-item";
+const CLEAR_BUTTON_CLASS = ".clear-button";
+const ERROR_CLASS = ".shortcut-error";
 /** 追加しているクラスのため.はなし */
 const IS_RECORDING_CLASS = "is-recording";
 const KEY_CLASS = ".key";
 // 初期処理
 setUpRecordButton();
+setUpClearButton();
 loadShortcutSettings();
 //　すべてのショートカットボタンにイベントを設定する
 function setUpRecordButton() {
@@ -18,16 +21,54 @@ function setUpRecordButton() {
     });
   }
 }
+// すべてのクリアボタンにイベントを設定する
+function setUpClearButton() {
+  const clearButtons = document.querySelectorAll(CLEAR_BUTTON_CLASS);
+  for (const clearButton of clearButtons) {
+    clearButton.addEventListener("click", () => {
+      const shortcutItem = clearButton.closest(SHORTCUT_ITEM_CLASS);
+      clearShortcutSetting(shortcutItem);
+    });
+  }
+}
 // ショートカットのレコードが開始されたら該当のshortcutItemにis-recordingクラスを追加する
 function startRecording(shortcutItem) {
+  // すでにrecord中のshortcutItemがある場合は、そちらのrecordを終了する
+  const recordingShortcutItem = getRecordingShortcutItem();
+  if (recordingShortcutItem) {
+    stopRecording(recordingShortcutItem);
+  }
   shortcutItem.classList.add("is-recording");
 }
+// ショートカットのレコードが終了したら該当のshortcutItemからis-recordingクラスを削除する
+function stopRecording(shortcutItem) {
+  shortcutItem.classList.remove("is-recording");
+  hideErrorMessages(shortcutItem);
+}
 // キーダウンイベント
-document.addEventListener("keydown", (event) => {
+document.addEventListener("keydown", async (event) => {
   const targetShortcutItem = getRecordingShortcutItem();
   if (!targetShortcutItem) {
     return;
   }
+  // ESCキーが押された場合はレコードをキャンセルする
+  if (event.key === "Escape") {
+    stopRecording(targetShortcutItem);
+    return;
+  }
+  // 修飾キーが押されている場合は無視する
+  if (event.ctrlKey || event.altKey || event.shiftKey || event.metaKey) {
+    return;
+  }
+  // すでに同じキーが設定されている場合は無視する
+  const actionName = await getActionName(event.key.toUpperCase());
+  if (actionName) {
+    const errorElement = targetShortcutItem.querySelector(ERROR_CLASS);
+    errorElement.classList.add("is-visible");
+
+    return;
+  }
+  hideErrorMessages(targetShortcutItem);
   const keyElement = targetShortcutItem.querySelector(KEY_CLASS);
   // ショートカットの設定を保存する
   saveShortcutSettings(
@@ -37,7 +78,7 @@ document.addEventListener("keydown", (event) => {
   keyElement.textContent = event.key.toUpperCase();
   targetShortcutItem.classList.remove("is-recording");
 });
-//
+// shortcutItemがrecord中かどうかを判定する関数
 function isRecording(shortcutItem) {
   return shortcutItem.classList.contains(IS_RECORDING_CLASS);
 }
@@ -67,4 +108,26 @@ function loadShortcutSettings() {
       }
     }
   });
+}
+// ショートカットの設定をクリアする関数
+function clearShortcutSetting(shortcutItem) {
+  const targetAction = shortcutItem.dataset.action;
+  chrome.storage.local.remove(targetAction);
+  const keyElement = shortcutItem.querySelector(KEY_CLASS);
+  keyElement.textContent = "Not set";
+}
+// キーからアクション名を取得する関数
+async function getActionName(key) {
+  const items = await chrome.storage.local.get(null);
+  for (const [action, value] of Object.entries(items)) {
+    if (value === key) {
+      return action;
+    }
+  }
+  return null;
+}
+// エラーメッセージを非表示にする関数
+function hideErrorMessages(shortcutItem) {
+  const errorElement = shortcutItem.querySelector(ERROR_CLASS);
+  errorElement.classList.remove("is-visible");
 }
